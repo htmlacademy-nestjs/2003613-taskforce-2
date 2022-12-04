9ea3e024c597bf3e7c5c59551ffaca9f316afc25
Artyom Petrov <artyom.petrov@gmail.com>
2022-12-04 18:07:52 +0300

    feat(request): create requests app WIP

---
+++ task-force/apps/requests/src/app/request/request.enity.ts
@@ -0,0 +1,27 @@
+import { Request } from '@task-force/shared-types';
+
+export default class RequestEntity implements Request {
+  public id: number;
+  public text: string;
+  public costProposal?: number;
+  public executorId: string;
+  public taskId: number;
+  public dateCreated: Date;
+
+  constructor(request: Request) {
+    this.fillEntity(request);
+  }
+
+  public toObject() {
+    return {...this};
+  }
+
+  public fillEntity(request: Request) {
+    this.id = request?.id;
+    this.text = request?.text;
+    this.costProposal = request?.costProposal;
+    this.executorId = request?.executorId;
+    this.taskId = request?.taskId;
+    this.dateCreated = request?.dateCreated;
+  }
+}
