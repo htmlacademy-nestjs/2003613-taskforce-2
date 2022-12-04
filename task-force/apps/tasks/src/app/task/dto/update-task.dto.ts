402547d2ca6515c1a8fb928fd072c31534242bb2
Artyom Petrov <artyom.petrov@gmail.com>
2022-12-04 18:09:55 +0300

    fix(tasks): change id type to number

--- task-force/apps/tasks/src/app/task/dto/update-task.dto.ts
+++ task-force/apps/tasks/src/app/task/dto/update-task.dto.ts
@@ -4,9 +4,9 @@ import { TaskStatus } from '@task-force/shared-types';
 export default class UpdateTaskDto {
   @ApiProperty({
     description: 'Valid task id',
-    example: 'd04eb35d-c36f-4e2b-b828-136379c7c6e3'
+    example: '4353642828136379763'
   })
-  public id: string;
+  public id: number;
 
   @ApiProperty({
     description: 'Any of possible task status',
