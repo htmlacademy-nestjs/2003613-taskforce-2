29f78212fefd218cb2193eb4bed79c3d513c69fe
Artyom Petrov <artyom.petrov@gmail.com>
2022-12-04 18:07:52 +0300

    feat(request): create requests app WIP

--- task-force/apps/requests/src/app/request/request.service.ts
+++ task-force/apps/requests/src/app/request/request.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import CreateRequestDto from './dto/create-request.dto';
+import RequestMemoryRepository from './request-memory.repository';
+import RequestEntity from './request.enity';
+
 
 @Injectable()
-export class RequestService {}
+export class RequestService {
+
+  constructor(
+    private readonly requestRepository: RequestMemoryRepository
+  ) {}
+
+  async create(dto: CreateRequestDto) {
+
+    const requestEntity = new RequestEntity(dto);
+
+    return this.requestRepository.create(requestEntity);
+  }
+
+
+  async index() {
+    return this.requestRepository.index();
+  }
+
+  async getRequestById(requestId: number) {
+    return this.requestRepository.findById(requestId);
+  }
+
+  async delete(requestId: number) {
+    await this.requestRepository.destroy(requestId);
+  }
+
+
+}
