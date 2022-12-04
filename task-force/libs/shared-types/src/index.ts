0baef00dd7ab8e0ae9a615d59dde877178719f56
Artyom Petrov <artyom.petrov@gmail.com>
2022-12-04 15:44:08 +0300

    feat(request): generate requests app WIP

--- task-force/libs/shared-types/src/index.ts
+++ task-force/libs/shared-types/src/index.ts
@@ -8,5 +8,6 @@ export * from './lib/task.interface';
 export * from './lib/task-category.interface';
 export * from './lib/task-status.enum';
 export * from './lib/task-tag.interface';
+export * from './lib/request.interface';
 export * from './lib/response.interface';
 export * from './lib/comment.interface';
