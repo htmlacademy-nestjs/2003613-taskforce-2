2f9ace4525363e48deaeb12a83f218e9871a56d6
Artyom Petrov <artyom.petrov@gmail.com>
2022-12-06 15:26:00 +0300

    feat(users): add groups validation to rdo, now one userRdo generate specified object output

--- task-force/libs/core/src/lib/helpers.ts
+++ task-force/libs/core/src/lib/helpers.ts
@@ -1,5 +1,13 @@
 import {plainToInstance, ClassConstructor} from 'class-transformer';
 
-export function fillObject<T, V>(dto: ClassConstructor<T>, plainObject: V) {
-  return plainToInstance(dto, plainObject, {excludeExtraneousValues: true});
+export function fillObject<T, V>(dto: ClassConstructor<T>, plainObject: V, groups: string[] = []) {
+  const options = (!groups.length)
+    ? {excludeExtraneousValues: true}
+    : {excludeExtraneousValues: true, groups: [...groups]};
+
+  return plainToInstance(dto, plainObject, {...options});
+}
+
+export function getMongoConnectionString({username, password, host, port, databaseName, authDatabase}): string {
+  return `mongodb://${username}:${password}@${host}:${port}/${databaseName}?authSource=${authDatabase}`;
 }
