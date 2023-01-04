b33c289d35242a23774ac4b1e1a9e422b4a4a2f5
Artyom Petrov <artyom.petrov@gmail.com>
2023-01-04 13:07:35 +0300

    feat(comments): configure PrismaORM for PostgreSql

--- taskforce/apps/comments/src/app/app.module.ts
+++ taskforce/apps/comments/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommentService } from './comment/comment.service';
 import { CommentController } from './comment/comment.controller';
 import { CommentModule } from './comment/comment.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CommentModule],
+  imports: [PrismaModule, CommentModule],
   controllers: [AppController, CommentController],
   providers: [AppService, CommentService],
 })
